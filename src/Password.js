dcc9c4d99f53618de93167ff82c16adc7b6dc902
GungunSharma131 <sgungun674@gmail.com>
2024-10-21 13:00:14 +0530

    update password and index css

--- src/Password.js
+++ src/Password.js
@@ -56,7 +56,7 @@ const handleCopy = useCallback(() => {
         <div className="container px-4">
         <div className="row gx-5">
         <div className="col input-box">
-        <marquee width="70%" direction="right" scrollamount="12" className="main-head">Password Generator</marquee>
+        <marquee width="100%" direction="right" scrollamount="12" className="main-head">Password Generator</marquee>
         <div className="input-group mb-3 main-box2">
        <input 
        id="floatingTextarea2"
@@ -77,44 +77,47 @@ const handleCopy = useCallback(() => {
        </button>
       
     </div>
-    <input
-     type="range"
-     className="main-range" 
-     value={Length}
-     onChange={(e) => {setLength(e.target.value)}}
-     readOnly
-     /><br/>
-    <label className="main-range1">Length: {Length}</label>
-    <div className="mb-3 form-check main-box5">
-
-    <input type="checkbox" 
-    className="form-check-input box" 
-    id="exampleCheck1"
-    defaultChecked = {Num}
-    onChange={()=>
-      {
-        setNum ((prev) => !prev)
-    }
-  }
-  readOnly
-    
-    />
-    <label className="form-check-label box-head" htmlFor="exampleCheck1">Number</label>
-  </div>
-  <div className="mb-3 form-check main-box6">
-    <input type="checkbox" 
-    className="form-check-input box1" 
-    id="exampleCheck1"
-
-    defaultChecked = {Char}
-    onChange={()=>
-      {
-        setChar ((prev) => !prev)
-    }
-  }
-    />
-    <label className="form-check-label box-head1" htmlFor="exampleCheck1">Characters</label>
-  </div>
+    <div className="password-feature ">
+      <div className="pass-range">
+        <input
+         type="range"
+         className=""
+         value={Length}
+         onChange={(e) => {setLength(e.target.value)}}
+         readOnly
+         /><br/>
+        <label className="">Length: {Length}</label>
+        </div>
+        <div className="form-check ">
+        <input type="checkbox"
+        className="form-check-input"
+        id="exampleCheck1"
+        defaultChecked = {Num}
+        onChange={()=>
+          {
+            setNum ((prev) => !prev)
+        }
+          }
+          readOnly
+        
+        />
+        <label className="form-check-label " htmlFor="exampleCheck1">Number</label>
+      
+        </div>
+        <div className=" form-check ">
+      <input type="checkbox"
+      className="form-check-input"
+      id="exampleCheck1"
+      defaultChecked = {Char}
+      onChange={()=>
+        {
+          setChar ((prev) => !prev)
+      }
+        }
+      />
+      <label className="form-check-label " htmlFor="exampleCheck1">Characters</label>
+        </div>
+    </div>
 
         </div>
         </div>
